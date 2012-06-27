dca70be5f517c86d4d2197e483384b1311cf26cc
DisplayCast screen sharing <displaycast@fxpal.com>
2012-06-27 14:27:09 -0700

    Reorgs

--- OSX/Faunus/Faunus.m
+++ OSX/Faunus/Faunus.m
@@ -1,10 +1,6 @@
-//
-//  Faunus.m
-//  Faunus
-//
-//  Created by Surendar Chandra on 6/27/12.
-//  Copyright (c) 2012 FX Palo Alto Lab. Inc. All rights reserved.
-//
+// Copyright (c) 2012, Fuji Xerox Co., Ltd.
+// All rights reserved.
+// Author: Surendar Chandra, FX Palo Alto Laboratory, Inc.
 
 #import "Faunus.h"
 
--- OSX/wallet.c
+++
@@ -1,9 +0,0 @@
-//
-//  wallet.c
-//  OSX
-//
-//  Created by Surendar Chandra on 6/26/12.
-//  Copyright (c) 2012 FX Palo Alto Lab. Inc. All rights reserved.
-//
-
-#include <stdio.h>
